eb1e4a181952c6def0ae4434608f62642030e836
madaraDance <maksim.minenko97@gmail.com>
2024-12-03 12:25:17 +0200

    created repoImpl for Company

---
+++ src/main/java/com/example/project_management_tool/infrastructure/CompanyRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.example.project_management_tool.infrastructure;
+
+import com.example.project_management_tool.domain.model.Company;
+import com.example.project_management_tool.domain.repository.company_abstraction.ICompanyRepository;
+import com.example.project_management_tool.domain.repository.company_abstraction.ICompanyRepositoryJpa;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class CompanyRepositoryImpl implements ICompanyRepository {
+    private final ICompanyRepositoryJpa iCompanyRepositoryJpa;
+
+    public CompanyRepositoryImpl(ICompanyRepositoryJpa iCompanyRepositoryJpa) {
+        this.iCompanyRepositoryJpa = iCompanyRepositoryJpa;
+    }
+
+    @Override
+    public Optional<Company> findOneById(UUID id) {
+        return iCompanyRepositoryJpa.findById(id);
+    }
+
+    @Override
+    public Company saveCompany(Company company) {
+        return iCompanyRepositoryJpa.save(company);
+    }
+}
