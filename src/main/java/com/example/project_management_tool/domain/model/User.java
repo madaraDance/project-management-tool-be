904068b8fac1106eb303a5e3421423c6e9f43583
madaraDance <maksim.minenko97@gmail.com>
2024-12-03 12:11:40 +0200

    created User

---
+++ src/main/java/com/example/project_management_tool/domain/model/User.java
@@ -0,0 +1,70 @@
+package com.example.project_management_tool.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "\"user\"")
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(
+        name = "first_name",
+        nullable = false
+    )
+    @NotBlank(message = "First Name can not be empty or null.")
+    @Size(
+        max = 25,
+        message = "First Name must be at most 25 characters")
+    private String firstName;
+
+    @Column(
+        name = "last_name",
+        nullable = false)
+    @NotBlank(message = "Last Name can not be empty or null.")
+    @Size(
+        max = 25,
+        message = "Last Name must be at most 25 characters")
+    private String lastName;
+
+    @Column(
+        name = "email",
+        nullable = false,
+        unique = true)
+    @NotBlank(message = "Email can not be empty or null.")
+    @Email(message = "Invalid email format")
+    @Size(
+            max = 75,
+            message = "First Name must be at most 75 characters")
+    private String email;
+
+    @Column(
+        name = "password",
+        nullable = false)
+    @NotBlank(message = "Password can not be empty or null.")
+    @Size(
+        max = 40,
+        min = 8,
+        message = "Password must be at least 8 characters and at most 40 characters")
+    private String password;
+
+    @Column(
+        name = "company_id",
+        nullable = false)
+    private UUID companyId;
+
+}
