16caba0736804e84a6fee8c5218fd1532c08de96
madaraDance <maksim.minenko97@gmail.com>
2024-12-08 13:49:31 +0200

    modified role enums and added defauld id for them

--- src/main/java/com/example/project_management_tool/domain/enums/SystemRole.java
+++ src/main/java/com/example/project_management_tool/domain/enums/SystemRole.java
@@ -1,6 +1,16 @@
 package com.example.project_management_tool.domain.enums;
 
 public enum SystemRole {
-    OWNER,
-    ADMIN
+    OWNER("123e4567-e89b-12d3-a456-426614000000"),
+    ADMIN("123e4567-e89b-12d3-a456-426614000001");
+
+    private final String uuid;
+
+    SystemRole(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
 }
