3bbfff6a471f36893c6397fc8e2dce987f8730ff
madaraDance <maksim.minenko97@gmail.com>
2024-12-16 14:12:25 +0200

    deleted unused imports

--- src/test/java/com/example/project_management_tool/integration/UserControllerTest.java
+++ src/test/java/com/example/project_management_tool/integration/UserControllerTest.java
@@ -11,7 +11,6 @@ import com.example.project_management_tool.presentation.config.JwtHelper;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.*;
